1f515f8362168f37116a8dff64465fe817ac5ca6
Gurpreet Singh <gurpreetsk@hotmail.com>
2019-02-11 17:29:29 +0530

    CsvReader, create file to parse a given csv from a given path.

---
+++ src/main/kotlin/com/gurpreetsk/CsvReader.kt
@@ -0,0 +1,36 @@
+package com.gurpreetsk
+
+import com.gurpreetsk.internal.FilePath
+import com.gurpreetsk.internal.ResourceKey
+import com.gurpreetsk.internal.ResourceValue
+import java.io.File
+
+class CsvReader(private val path: FilePath) {
+    fun parseCsv(): Map<ResourceKey, ResourceValue> {
+        val file = getReadableFile()
+        return if (file.canRead()) {
+            readFileContents(file)
+        } else {
+            throw FileSystemException(file, reason = "File not readable")
+        }
+    }
+
+    private fun getReadableFile(): File {
+        return File(path.value)
+            .apply { setReadable(true) }
+    }
+
+    private fun readFileContents(
+        file: File
+    ): Map<ResourceKey, ResourceValue> {
+        val map = mutableMapOf<ResourceKey, ResourceValue>()
+        file.useLines { sequence ->
+            sequence.iterator().forEach { line ->
+                val keyValuePairs = line.split(",")
+                map[ResourceKey(keyValuePairs[0])] = ResourceValue(keyValuePairs[1])
+            }
+        }
+
+        return map.toMap()
+    }
+}
