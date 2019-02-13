bf834e7d41bc1ea53b0a120fbd51584ed9b4abcf
Gurpreet Singh <gurpreetsk@hotmail.com>
2019-02-13 18:06:12 +0530

    FileWriter, end generated file with a newline.

--- src/main/kotlin/com/gurpreetsk/FileWriter.kt
+++ src/main/kotlin/com/gurpreetsk/FileWriter.kt
@@ -10,10 +10,12 @@ class FileWriter {
     fun createAndroidStringsFile(parentDirectory: File, contents: String) {
         val androidStrings = File(parentDirectory, "strings.xml")
         androidStrings.writeText(contents)
+        androidStrings.appendText("\n") // End file with a new line.
     }
 
     fun createiOSStringsFile(parentDirectory: File, contents: String) {
         val iOSStrings = File(parentDirectory, "localizable.strings")
         iOSStrings.writeText(contents)
+        iOSStrings.appendText("\n") // End file with a new line.
     }
 }
